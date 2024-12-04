fcadb20b48e7fc297922130b93d71063a9975ca3
Josh Johanning <joshjohanning@github.com>
2024-12-03 22:08:35 -0600

    opening dontsleep in background

--- dontsleep.bat
+++ dontsleep.bat
@@ -1,4 +1,4 @@
-schtasks /create /SC ONCE /TN dontsleep /TR "'D:\Backups\Joshs HDD\Programs\DontSleep_x64.exe'" /ST 00:00
+schtasks /create /SC ONCE /TN dontsleep /TR "'D:\Backups\Joshs HDD\Programs\DontSleep_x64.exe' -bg" /ST 00:00
 schtasks /run /TN dontsleep
 schtasks /delete /TN dontsleep /F
 pause
