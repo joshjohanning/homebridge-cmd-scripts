42fc74b00601e82512106157a1ca742db7a1163b
Josh Johanning <joshjohanning@github.com>
2025-02-09 15:09:44 -0600

    removing old IP reference

--- sleep.sh
+++ sleep.sh
@@ -7,7 +7,6 @@ fi
 user=$1
 ip=$2
 
-# ssh Joshua@192.168.1.50 -- 'taskkill /IM "DontSleep_x64.exe" /F & "C:\Users\Joshua\Dropbox\Documents\psshutdown64.exe" -d -t 0 -accepteula' > /home/pi/scripts/test/log.txt
 timeout 5 ssh $user@$ip << EOF
   taskkill /IM "DontSleep_x64.exe" /F
   "C:\Users\Joshua\Dropbox\Documents\Josh's Folder\psshutdown64.exe" -d -t 0 -accepteula
